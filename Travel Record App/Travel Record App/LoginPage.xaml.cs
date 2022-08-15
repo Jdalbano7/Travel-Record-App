660c08a8a454fccc9e30440f47c48ff72c0fc8a3
Joshua <Joshuadalbano@yahoo.com>
2022-08-15 10:02:11 -0400

    Made loginPage with login and started Map page, history page, and profile page

---
+++ Travel Record App/Travel Record App/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Travel_Record_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(entryEmail.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(entryPassword.Text);
+
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+                DisplayAlert("Email or Password is Empty", "Please enter a password and email", "Got it");
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
--- Travel Record App/Travel Record App/MapPage.xaml.cs
+++ Travel Record App/Travel Record App/MapPage.xaml.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Travel_Record_App
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
     {
-        public MainPage()
+        public MapPage()
         {
             InitializeComponent();
-        }
-
-        private void btnLogin_Clicked(object sender, EventArgs e)
-        {
-            
         }
     }
 }
