fd575210929e8806e687875d215ea811a2a3154e
Joshua <Joshuadalbano@yahoo.com>
2022-08-15 13:45:38 -0400

    Added sqlite nuget, added App ctor with DatabaseLocation and string DatabaseLocation

--- Travel Record App/Travel Record App/App.xaml.cs
+++ Travel Record App/Travel Record App/App.xaml.cs
@@ -6,11 +6,21 @@ namespace Travel_Record_App
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string dataBaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+            
+            DatabaseLocation = dataBaseLocation;
         }
 
         protected override void OnStart()
