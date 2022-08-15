4ae13ffa4f8815085de6e84f15c8abfcd427e6ef
Joshua <Joshuadalbano@yahoo.com>
2022-08-15 16:47:34 -0400

    Added Post class and Model folder

---
+++ Travel Record App/Travel Record App/Model/Post.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel_Record_App.Model
+{
+    public class Post
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; } //Needed for sqlite. Allows us to identify an element from one another.
+                                    //int is the default type for sqlite
+        [MaxLength (300)]
+        public string Experience { get; set; }
+
+
+    }
+}
