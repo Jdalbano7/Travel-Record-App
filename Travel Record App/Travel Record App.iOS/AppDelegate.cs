93459a6062af5a5e052da5261e6bdcbc740b9721
Joshua <Joshuadalbano@yahoo.com>
2022-08-17 09:29:03 -0400

    Added xamarin.forms.maps nuget and got IOS ready to use maps. Added the privacy location to info.plist

--- Travel Record App/Travel Record App.iOS/AppDelegate.cs
+++ Travel Record App/Travel Record App.iOS/AppDelegate.cs
@@ -15,6 +15,7 @@ namespace Travel_Record_App.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
 
             string dbName = "travel_db.sqlite";
             //needed for iOS
