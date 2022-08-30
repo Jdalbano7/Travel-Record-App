472219167cd2184debb10de08357898c67f4722c
Joshua <Joshuadalbano@yahoo.com>
2022-08-30 10:16:54 -0400

    Made constants Venue search, client id, client secret, and added a generate URL method

---
+++ Travel Record App/Travel Record App/Helpers/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel_Record_App.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH =
+            "https://api.foursquare.com/v2/venus/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "LIIJ2SDEDUSVE1B45PMCOAVW0FNLRMZI2D4HNVL0OH2O1R4U\r\n\r\n";
+        public const string CLIENT_SECRET = "MROBTKC2TX0NZZI23E3ESGFXZN4NZUQVTSVRYOANCGRPIFEP";
+    }
+}
---
+++ Travel Record App/Travel Record App/Model/Venue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Travel_Record_App.Helpers;
+
+namespace Travel_Record_App.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitude, double longitude)
+        {
+           return string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
